e4d695503334bee5e8007d242419b2eb57b0743d
Neto Jocelino <netojocelino@gmail.com>
2020-03-17 22:11:56 -0300

    Rotas adicionadas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
+use App\Http\Controllers;
+
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+	return array(
+		'status' => 200,
+		'message' => 'success',
+		'using' => $router->app->version()
+	);
 });
+
+
+$router->group(['prefix' => 'boards'], function() use($router) {
+	$router->get('/', "BoardController@list");
+	$router->get('/{id}', "BoardController@details");
+	$router->post('/', "BoardController@new");
+	$router->put('/{id}', "BoardController@undone");
+	$router->delete('/{id}', "BoardController@done");
+});
+
+$router->group(['prefix' => 'cards'], function() use($router) {
+	$router->get('/', "CardController@list");
+	$router->get('/{id}', "CardController@details");
+	$router->post('/', "CardController@new");
+	$router->put('/{id}/move', "CardController@move");
+	$router->put('/{id}', "CardController@undone");
+	$router->delete('/{id}', "CardController@done");
+});
+// $router->post('/cards/new', "CardController@new");
+// $router->post('/cards/{id}/done', "CardController@done");
